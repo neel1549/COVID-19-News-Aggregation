7951196266780005532fb610da8d8ad467e0c8e9
Neel Kattumadam <neelkk@gmail.com>
2020-06-12 14:51:06 -0700

    Removed Splash

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,34 +1,18 @@
-import React from 'react';
-import SplashScreen from './components/SplashScreen'
-import {Router,Route,Switch} from 'react-router-dom';
-import history from './history';
-import HomeScreen from './components/HomeScreen'
+import React from "react";
+import SplashScreen from "./components/SplashScreen";
+import { Router, Route, Switch } from "react-router-dom";
+import history from "./history";
+import HomeScreen from "./components/HomeScreen";
 
-
-const App= function(props) {
-    return (
+const App = function (props) {
+  return (
     <div>
-        <Router history={history}>
-            <Switch>
-                <Route path= "/" exact component={SplashScreen}/>
-                <Route path= "/home" exact component={HomeScreen}/>
-       
-
-            </Switch>
-
-
-        </Router>
-        
-        
-
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={HomeScreen} />
+        </Switch>
+      </Router>
     </div>
-    );
-
-
-
-
-}
+  );
+};
 export default App;
-
-
-
